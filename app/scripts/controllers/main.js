8b621b122cce33e04815be0e173192d86f70bb6c
Manikandan Nagarajan <manikandan_nagarajan@ymail.com>
2015-11-29 11:46:52 +0530

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eyeCheckerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the eyeCheckerApp
+ */
+angular.module('eyeCheckerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
